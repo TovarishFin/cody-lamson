f256d5bf45b7c2f4aa0c47f25c9be0afab3f0cb2
TovarishFin <tovarishfin@gmail.com>
2019-02-05 23:56:40 +0100

    crappy workaround for image loading (cannot be dynamic)

--- src/components/LogoGrid.vue
+++ src/components/LogoGrid.vue
@@ -2,16 +2,213 @@
   <v-card class="mb-4 pa-4" elevation="4">
     <v-container grid-list-sm align-center justify-center>
       <v-layout row wrap align-center justify-center>
-        <v-flex
-          v-for="(logo, i) in logos"
-          :key="i"
-          xs3
-          md2
-          xl1
-          style="text-align: center;"
-        >
+        <v-flex xs3 md2 xl1 style="text-align: center;">
           <v-avatar>
-            <g-image :src="`/images/logos/${logo}`" class="image" />
+            <g-image src="/images/logos/angular.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/bitcoin.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/bootstrap.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/chai.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/combo.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/css.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/drizzle.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/ethereum.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/firebase.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/ganache.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/golang.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/gulp.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/html.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/javascript.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/mac.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/materialui.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/metamask.svg" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/mocha.svg" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/mongo.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/node.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/npm.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/polymer.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/python.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/raiden.svg" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/react.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/redux.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/solidity.svg" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/truffle.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/ubuntu.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/webpack.svg" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/yarn.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/vue.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/cpp.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/eos.png" class="image" />
+          </v-avatar>
+        </v-flex>
+
+        <v-flex xs3 md2 xl1 style="text-align: center;">
+          <v-avatar>
+            <g-image src="/images/logos/oraclize.png" class="image" />
           </v-avatar>
         </v-flex>
       </v-layout>
