9c40c3933792680e291fb558649427d76e051751
TovarishFin <tovarishfin@gmail.com>
2019-02-06 00:58:30 +0100

    attempt at prod error handling

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 module.exports = {
   configureWebpack: {
     plugins: [new VuetifyLoaderPlugin()]
+  },
+  errorHandler: (err, vm, info) => {
+    console.error(err, vm, info)
   }
 }
