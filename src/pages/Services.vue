96d94d8147ea88a05b52c7430d38636dc6547753
TovarishFin <tovarishfin@gmail.com>
2019-02-06 10:53:01 +0100

    prod being annoying

---
+++ src/pages/Services.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <v-card>
+      <v-card-text>
+        <p class="display-1">Services Offered</p>
+      </v-card-text>
+    </v-card>
+  </Layout>
+</template>
+<script>
+export default {
+  metaInfo: {
+    title: 'Services offered by Cody Lamson',
+    meta: [
+      {
+        key: 'description',
+        name: 'description',
+        content: 'Find out what Cody Lamson can do for you'
+      }
+    ]
+  }
+}
+</script>
