3107929186a012782450edb6c9e8be442e82f747
TovarishFin <tovarishfin@gmail.com>
2019-02-06 11:16:50 +0100

    services tweak

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,7 +1,19 @@
 <template>
-  <p>hello</p>
-
+  <Layout>
+    <p class="display-4">sup buddy</p>
+  </Layout>
 </template>
 <script>
-
+export default {
+  metaInfo: {
+    title: 'Services offered by Cody Lamson',
+    meta: [
+      {
+        key: 'description',
+        name: 'description',
+        content: 'Find out what Cody Lamson can do for you'
+      }
+    ]
+  }
+}
 </script>
